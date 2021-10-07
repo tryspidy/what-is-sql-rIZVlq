b24f3ed20bc715c333970dbd1a8d687ff6179351
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-07 18:43:30 +0530

    Initial commit

---
+++ main.sql
@@ -0,0 +1,20 @@
+SQL stands for:
+Structured Query Language
+
+It's language which enables a user to create,
+modify and essentially interact with a database.
+As a tester we are usually using Data manipulation
+language part of SQL
+
+SQL has 2 parts:
+1-DATA MANIPULATION LANGUAGE
+It is used to add, retrieve or update the data.
+= insert, select, update, delete
+
+2-DATA DEFINITION LANGUAGE
+It is used to create database schema and can
+be used to define some constraints as well.	
+=create, alter, drop, truncate
+
+create table
+create window etc.
